24fc90ce8beaea5a36ef9f0ee8310a9ebab362d1
packsnicht <marcus.migotti@gmx.net>
2021-12-01 13:20:43 +0100

    Add files via upload

--- Day01.cs
+++ Day01.cs
@@ -29,8 +29,8 @@ namespace Day01
             int count = 0;
             for (int i = 1; i < input.Length - 2; i++)
             {
-                var previous = input[i - 1] + input[i] + input[i + 1];
-                var current = input[i] + input[i + 1] + input[i + 2];
+                int previous = input[i - 1] + input[i] + input[i + 1];
+                int current = input[i] + input[i + 1] + input[i + 2];
 
                 if (previous < current) count++;
             }
