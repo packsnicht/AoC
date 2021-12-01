90d58dcb31a41aa8517b25707cc0d2aa1541f6d3
packsnicht <marcus.migotti@gmx.net>
2021-12-01 12:24:43 +0100

    Add files via upload

---
+++ Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Util
+{
+    public static class Data
+    {
+        public static int[] ToIntArray(string file, char delimiter = '\n')
+        {
+            return File.ReadAllText(@file).Split(delimiter)                
+                    .Select(x => int.Parse(x))
+                    .ToArray();
+        }
+    }
+}
---
+++ Day01.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Util;
+
+namespace Day01
+{
+    class Day01
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Data.ToIntArray("D:/AoC/Data/01.txt", '\n');
+
+            Console.WriteLine(Part01(input)); 
+            Console.WriteLine(Part02(input));
+
+        }
+
+        static int Part01(int[] input)
+        {
+            int increased = 0;
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i - 1] < input[i]) increased++;
+            }
+            return increased;
+        }
+
+        static int Part02(int[] input)
+        {
+            int increased = 0;
+            for (int i = 1; i < input.Length - 2; i++)
+            {
+                var previous = input[i - 1] + input[i] + input[i + 1];
+                var current = input[i] + input[i + 1] + input[i + 2];
+
+                if (previous < current) increased++;
+            }
+            return increased;
+        }
+    }
+}
